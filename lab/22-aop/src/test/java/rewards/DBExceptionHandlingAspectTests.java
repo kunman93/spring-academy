e71db1b8acd7ce83852f7875d0027df3feb8d075
kunman93 <kun.manu93@gmail.com>
2025-04-04 10:56:38 +0200

    feat: complete coding assignment of module 6
    - add @ComponentScan and @EnableAspectJAutoProxy to AspectConfig
    - modify the @Import to include AspectsConfig.class in the
      SystemTestConfig configuration class
    - annotate and wire the Logging Aspect with @Aspect and @Component
    - define @Before advice with its pointcut expression
    - define @Around advice with its pointcut expression
    - define @AfterThrowing advice with its pointcut expression

--- lab/22-aop/src/test/java/rewards/DBExceptionHandlingAspectTests.java
+++ lab/22-aop/src/test/java/rewards/DBExceptionHandlingAspectTests.java
@@ -16,7 +16,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { DbExceptionTestConfig.class })
+@ContextConfiguration(classes = {DbExceptionTestConfig.class})
 @EnableAutoConfiguration
 public class DBExceptionHandlingAspectTests {
 
@@ -26,21 +26,14 @@ public class DBExceptionHandlingAspectTests {
     @Test
     @CaptureSystemOutput
     public void testReportException(OutputCapture capture) {
-    	
-    	// The repository.findByCreditCard(..) method below will 
-    	// result in an exception because we are using empty database
-    	// set by DbExceptionTestConfig configuration class
-    	// used by @ContextConfiguration annotation above.
+
+        // The repository.findByCreditCard(..) method below will
+        // result in an exception because we are using empty database
+        // set by DbExceptionTestConfig configuration class
+        // used by @ContextConfiguration annotation above.
         assertThrows(RewardDataAccessException.class, () -> {
             repository.findByCreditCard("1234123412341234");
         });
-
-        // TODO-12: (Optional) Validate our AOP is working.
-        //
-        // - An error message should now be logged to the console as a warning
-        // - Save all your work and run this test - it should pass with a warning
-        //   message on the console AND the console output assertion (below)
-        //   should succeed.
 
         if (TestConstants.CHECK_CONSOLE_OUTPUT) {
             assertThat(capture.toString(), containsString(DBExceptionHandlingAspect.EMAIL_FAILURE_MSG));
