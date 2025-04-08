3d9007c1da08bb3a9e471ad96f4600af10c18009
kunman93 <kun.manu93@gmail.com>
2025-04-08 11:06:37 +0200

    feat: complete coding assignment of module 8
    - refactor: use jdbcTemplate
    - refactor: inject jdbcTemplate directly
    	- inject jdbcTemplate direclty into Jdbc*Repository

--- lab/26-jdbc/src/main/java/config/RewardsConfig.java
+++ lab/26-jdbc/src/main/java/config/RewardsConfig.java
@@ -1,11 +1,8 @@
 package config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -15,36 +12,37 @@ import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.JdbcRewardRepository;
 import rewards.internal.reward.RewardRepository;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class RewardsConfig {
+    private final JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	DataSource dataSource;
-		
-	@Bean
-	public RewardNetwork rewardNetwork(){
-		return new RewardNetworkImpl(
-			accountRepository(), 
-			restaurantRepository(), 
-			rewardRepository());
-	}
-	
-	@Bean
-	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository(dataSource);
-		return repository;
-	}
-	
+    public RewardsConfig(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public RewardNetwork rewardNetwork() {
+        return new RewardNetworkImpl(
+            accountRepository(),
+            restaurantRepository(),
+            rewardRepository());
+    }
+
+    @Bean
+    public AccountRepository accountRepository() {
+        return new JdbcAccountRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public RestaurantRepository restaurantRepository() {
+        return new JdbcRestaurantRepository(jdbcTemplate);
+    }
+
+    @Bean
+    public RewardRepository rewardRepository() {
+        return new JdbcRewardRepository(jdbcTemplate);
+    }
+
 }
