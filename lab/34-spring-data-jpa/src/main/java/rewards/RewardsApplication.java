bc4535d9e49346aa4b35b9ca44198d1aaa7d836d
kunman93 <kun.manu93@gmail.com>
2025-04-14 18:34:11 +0200

    feat: complete Spring Boot coding assignment 3
    - define Restaurant class as an entity
    - extend *Repostory with Spring Data Repository
            - refactor, rename method to obey Spring Data conventions
    - configure JPA for the tests

--- lab/34-spring-data-jpa/src/main/java/rewards/RewardsApplication.java
+++ lab/34-spring-data-jpa/src/main/java/rewards/RewardsApplication.java
@@ -10,42 +10,31 @@ import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-// TODO-00: In this lab, you are going to exercise the following:
-// - Writing Spring Data repository interfaces
-// - Annotating a JPA Entity class
-// - Setting some JPA related properties
-
-// TODO-01: Review dependency as described in the TO-DO-01 
-//          in the pom.xml or build.gradle 
 
 @SpringBootApplication
 @Import(RewardsConfig.class)
 public class RewardsApplication {
-	private final Logger logger = LoggerFactory.getLogger(RewardsApplication.class);
+    private final Logger logger = LoggerFactory.getLogger(RewardsApplication.class);
 
-	private static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
+    private static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
-	public static void main(String[] args) {
-		SpringApplication.run(RewardsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RewardsApplication.class, args);
+    }
 
-	@Component
-	public final class QueryAccountCountRunner implements CommandLineRunner {
+    @Component
+    public final class QueryAccountCountRunner implements CommandLineRunner {
 
-		private JdbcTemplate jdbcTemplate;
+        private final JdbcTemplate jdbcTemplate;
 
-		public QueryAccountCountRunner(JdbcTemplate jdbcTemplate) {
-			this.jdbcTemplate = jdbcTemplate;
-		}
+        public QueryAccountCountRunner(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
 
-		@Override
-		public void run(String... args) throws Exception {
-			long accountCount = this.jdbcTemplate.queryForObject(SQL, Long.class);
-			logger.info("Number of accounts:{}", accountCount);
-		}
-	}
+        @Override
+        public void run(String... args) throws Exception {
+            long accountCount = this.jdbcTemplate.queryForObject(SQL, Long.class);
+            logger.info("Number of accounts:{}", accountCount);
+        }
+    }
 }
-
-// TODO-07: Configure JPA as specified in the TO-DO-07 in the
-//          src/test/resources/application.properties
-//          ("application.properties" file used for testing)
